21aba5052d440941865e6624dc21339891185286
patrick jones <patrick@patrickjones.ca>
2011-11-06 15:06:41 -0500

    more shifing around - plus changed class of main interface to a listActivity.

--- gen/ca/pjones/app/R.java
+++ gen/ca/pjones/app/R.java
@@ -14,23 +14,18 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int EditText01=0x7f050001;
-        public static final int button1=0x7f050004;
-        public static final int checkBox1=0x7f050006;
-        public static final int checkBox2=0x7f050007;
-        public static final int editText1=0x7f050003;
-        public static final int radioButton1=0x7f050002;
-        public static final int radioButton2=0x7f050005;
         public static final int relativeLayout1=0x7f050000;
+        public static final int textView1=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int splash=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int decode=0x7f040004;
         public static final int encode=0x7f040002;
         public static final int hello=0x7f040000;
+        public static final int splash=0x7f040005;
         public static final int translate=0x7f040003;
     }
 }
